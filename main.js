2894446adbf02357bdf44888bfc9d220efb29cf7
Davis Engeler <davisengeler@users.noreply.github.com>
2024-01-25 22:05:22 -0500

    Improve ipfs integration

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
 const { exec } = require('child_process');
+const windowStateKeeper = require('electron-window-state');
 
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV === 'dev';
 const isMac = process.platform === 'darwin';
 
 function createMainWindow() {
+    let mainWindowState = windowStateKeeper({
+        defaultWidth: 1000,
+        defaultHeight: 800
+    });
+
     const mainWindow = new BrowserWindow({
         title: 'memeSRC', // Fixed typo here from 'titls' to 'title'
-        width: isDev ? 1400 : 1250,
-        height: 750,
+        x: mainWindowState.x,
+        y: mainWindowState.y,
+        width: mainWindowState.width,
+        height: mainWindowState.height,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
@@ -25,8 +33,7 @@ function createMainWindow() {
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
     // mainWindow.loadURL('https://beta.memesrc.com/')
 
-    // Example command line interaction
-    runSystemCommand();
+    mainWindowState.manage(mainWindow);
 }
 
 function runSystemCommand() {
